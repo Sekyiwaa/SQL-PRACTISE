c206ac7ff04d048f40139ea3652fa143be86f6ff
Akua Sekyiwaa Osei-Nkwantabisa <oseinkwantabisaakua@gmail.com>
2024-05-22 15:18:06 -0500

    new update

---
+++ EASY/Aggregate Listening Data-Spotify.sql
@@ -0,0 +1,7 @@
+SELECT
+    user_id,
+    ROUND(SUM(IFNULL(listen_duration,0))/60.0) AS 
+    total_listen_duration,
+    COUNT(DISTINCT song_id) AS unique_song_count
+FROM listening_habits
+GROUP BY user_id;
