72a6b6754c22496b7e9d70f2c2875aec881f5cde
Akua Sekyiwaa Osei-Nkwantabisa <oseinkwantabisaakua@gmail.com>
2025-03-09 13:01:31 -0500

    Another Python Code

---
+++ EASY/Number Of Unique Facilities And Inspections Per Municipality-CITY OF LOS ANGELES.sql
@@ -0,0 +1,10 @@
+SELECT
+    facility_zip,
+    COUNT(DISTINCT facility_id) AS no_facilities,
+    COUNT(*) AS no_inspections
+FROM
+    los_angeles_restaurant_health_inspections
+GROUP BY 
+    facility_zip
+ORDER BY 
+    no_inspections DESC
