95e47e5acd4ce2ce0c84b5e61b749f1aa45a544c
Akua Sekyiwaa Osei-Nkwantabisa <oseinkwantabisaakua@gmail.com>
2025-03-24 19:00:34 -0500

    new code

---
+++ HARD/Monthly Percentage Difference.sql-AMAZON.sql
@@ -0,0 +1,7 @@
+SELECT DATE_FORMAT(created_at,'%Y-%m') AS ym,
+       ROUND((SUM(value) - LAG(SUM(value)) OVER ())
+             / LAG(SUM(value)) OVER ()
+             * 100, 2) AS revenue_diff_pct
+  FROM sf_transactions
+ GROUP BY ym
+ ORDER BY ym
